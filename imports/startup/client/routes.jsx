c422797918ed8ee60779ce568bb9cadddb255003
Mason <masonlgoetz@gmail.com>
2016-12-28 20:05:43 -0700

    changed routes to obj style

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -11,17 +11,48 @@ import ProfilePage from '../../ui/ProfilePage'
 // not found
 import { NotFound } from '../../ui/pages/not-found'
 
-// react router way
+const routes = {
+  path: '/',
+  component: AppLayout,
+  indexRoute: { component: TaskBoard },
+  childRoutes: [
+    { path: 'profile', component: ProfilePage },
+    { path: '*', component: NotFound}
+    // {
+    //   path: 'inbox',
+    //   component: Inbox,
+    //   childRoutes: [{
+    //     path: 'messages/:id',
+    //     onEnter: ({ params }, replace) => replace(`/messages/${params.id}`)
+    //   }]
+    // },
+    // {
+    //   component: Inbox,
+    //   childRoutes: [{
+    //     path: 'messages/:id', component: Message
+    //   }]
+    // }
+  ]
+}
+
+// react router way #1
 Meteor.startup( () => {
   render(
-    <Router history={ browserHistory }>
-      <Route path='/' component={ AppLayout }>
-        <IndexRoute component={ TaskBoard } />
-        <Route path='/profile' component={ ProfilePage } />
-        {/* similar to express, this 404 route needs to be the last route (its a last resort) */}
-        <Route path='*' component={ NotFound } />
-      </Route>
-    </Router>,
-    document.getElementById( 'react-root' )
-  );
-});
+    <Router history={ browserHistory } routes={ routes } />, document.getElementById( 'react-root' )
+  )
+})
+
+// react router way #2
+// Meteor.startup( () => {
+//   render(
+//     <Router history={ browserHistory }>
+//       <Route path='/' component={ AppLayout }>
+//         <IndexRoute component={ TaskBoard } />
+//         <Route path='/profile' component={ ProfilePage } />
+//         {/* similar to express, this 404 route needs to be the last route (its a last resort) */}
+//         <Route path='*' component={ NotFound } />
+//       </Route>
+//     </Router>,
+//     document.getElementById( 'react-root' )
+//   )
+// })
